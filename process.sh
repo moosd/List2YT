2ba1f27afd2364c31fcdc6c71e9d761d0821cfc2
moosd <souramoo@gmx.com>
2015-11-17 20:01:39 +0000

    Added list of videos indicator

--- process.sh
+++ process.sh
@@ -9,8 +9,8 @@ fi
 
 while read txt; do
     list2+=$(wget -O - "https://www.youtube.com/results?search_query=$txt" | grep "watch?" | cut -d'"' -f6 | grep -v list | grep watch | cut -d'=' -f2 | sed -n '1 p')
-    list2+="\n"
+    list2+=$'\n'
 done<<<"$(cat $file)"
 
 cat src.html > index.html
-echo "<script>data = \"$(echo $list2|base64 -w 0)\"</script></body></html>" >> index.html
+echo "<script>data = \"$(echo $list2|base64 -w 0)\", namesd = \"$(cat $file|base64 -w 0)\"</script></body></html>" >> index.html
--- rawlist.sh
+++ rawlist.sh
@@ -7,4 +7,4 @@ if [ "$#" -ne 1 ]; then
 fi
 
 cat src.html > index.html
-echo "<script>data = \"$(cat $file|cut -d'=' -f2|base64 -w 0)\"</script></body></html>" >> index.html
+echo "<script>data = \"$(cat $file|cut -d'=' -f2|base64 -w 0)\", namesd = \"$(cat $file|cut -d'=' -f2|base64 -w 0)\"</script></body></html>" >> index.html
